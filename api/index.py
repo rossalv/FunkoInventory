5da4e1c82b3620e270b0d0cabc0b2aa163716256
Ross Alvine <rfalvine@gmail.com>
2023-07-13 21:09:53 -0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -14,8 +14,34 @@ def home():
     session = tls_client.Session(
     client_identifier='safari_ios_15_6',
     random_tls_extension_order=True
-    )
-    return str(session)
+        )
+    lo = 0
+    hi = 20000
+    lastmid = -1
+    data = {}
+    while True:
+        mid = (lo + hi) // 2
+        if mid == lastmid:
+            stock = mid
+            break
+        cache = randomStringDigits(12)
+        page = session.get('https://funko.com/on/demandware.store/Sites-FunkoUS-Site/en_US/Product-ShowQuickView?pid=' + str(pid) + '&quantity=' + str(mid) + '&cache=' + cache)
+        data = page.json()
+        availability = data['product']['availability']['messages'][0]
+        lastmid = mid
+        if availability == 'This item is currently not available':
+            stock = '0'
+            #print(f'Stock: {stock}')
+            break
+        elif availability == '*** Item(s) in Stock':
+            hi = mid
+        else:
+            lo = mid
+
+    name = str(data['product']['productName'])
+    price = str(data['product']['price']['sales']['formatted'])
+    produrl = 'https://funko.com/ralvin/' + str(pid) + '.html'
+    return f'{name} - {stock}'
 
 @app.route('/about')
 def about():
