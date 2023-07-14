fe16d27fb70e7139e1ae4599503ffbb8ad92b2e2
Ross Alvine <rfalvine@gmail.com>
2023-07-13 21:01:30 -0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -2,9 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
+def randomStringDigits(stringLength=6):
+    lettersAndDigits = string.ascii_letters + string.digits
+    return ''.join(random.choice(lettersAndDigits) for i in range(stringLength))
+
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return randomStringDigits(12)
 
 @app.route('/about')
 def about():
