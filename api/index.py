390996e966b88a85965875d082d4aa2b53accb97
Ross Alvine <rfalvine@gmail.com>
2024-04-10 11:51:14 -0600

    Update index.py

--- api/index.py
+++ api/index.py
@@ -8,13 +8,9 @@ app = Flask(__name__)
 def randomStringDigits(stringLength=6):
     lettersAndDigits = string.ascii_letters + string.digits
     return ''.join(random.choice(lettersAndDigits) for i in range(stringLength))
-
+    
 @app.route('/')
 def home():
-    return 'Stock Checker'
-    
-@app.route('/stock')
-def stock():
     pid = request.args.get('pid')
     session = tls_client.Session(
     client_identifier='safari_ios_15_6',
